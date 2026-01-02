7d5f8abe289a1ba98191c6f7a527b4abfc19cb84
Prayog <prayoglide@gmail.com>
2026-01-02 09:19:36 +0545

    Update Meta.js

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -1,19 +1,21 @@
 import Head from "next/head"
-import React from "react"
 import { useSettings } from "@/context/settings"
 
 const Meta = () => {
 	const { settings } = useSettings()
-	const title = settings.title || "Home"
-	
-	// Moving the long string here prevents formatting errors
+
+	if (!settings) return null
+
+	const title = settings.title ?? "Home"
+	const description = `Start page of ${settings.username ?? "user"}`
+
 	const blankIcon =
 		"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII="
 
 	return (
 		<Head>
 			<title>{title}</title>
-			<meta name="description" content={`Start page of ${settings.username}`} />
+			<meta name="description" content={description} />
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
 			<link rel="icon" href={blankIcon} />
 			<meta name="robots" content="noindex, nofollow" />
